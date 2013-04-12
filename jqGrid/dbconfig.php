f8560ffa36229c00158bca188d28f6a0a5976958
vishnun <vishnun@thoughtworks.com>
2013-04-12 11:12:24 +0530

    Vishnu - Basic spike working using jqGrid.

---
+++ jqGrid/dbconfig.php
@@ -0,0 +1,12 @@
+<?php
+  // Connection's Parameters
+  $db_host="127.0.0.1";
+  $db_name="test";
+  $username="root";
+  $password="";
+  //$db_con=mysql_connect($db_host,$username,$password);
+  //$connection_string=mysql_select_db($db_name);
+  // Connection
+  //mysql_connect($db_host,$username,$password);
+  //mysql_select_db($db_name);
+?>
